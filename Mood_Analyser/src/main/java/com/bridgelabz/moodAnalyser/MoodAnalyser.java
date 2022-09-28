bd01665e15ab3f64e29f3cbbbeee90011c7300df
shreyashdj <shreyashjadhav1799@gmail.com>
2022-09-28 21:43:46 +0530

    [Shreyash]: Refactored code to add constructor with parameter message of String type

--- Mood_Analyser/src/main/java/com/bridgelabz/moodAnalyser/MoodAnalyser.java
+++ Mood_Analyser/src/main/java/com/bridgelabz/moodAnalyser/MoodAnalyser.java
@@ -3,6 +3,13 @@ package com.bridgelabz.moodAnalyser;
  *  @author - Shreyash Jadhav
  */
 public class MoodAnalyser {
+    private String message;    // variable message of String type
+    /**
+      * created constructor with parameter message of String type
+      */
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
     /**
      *  creating method analyseMood having string return type and  parameter of string type
      */
