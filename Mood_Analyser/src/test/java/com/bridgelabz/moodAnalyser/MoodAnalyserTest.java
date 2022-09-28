239f32a849dd3037f084515bb8aa231a34ca7ca4
shreyashdj <shreyashjadhav1799@gmail.com>
2022-09-28 21:19:25 +0530

    [Shreyash]: Modified code to Test analyseMood method for Any mood and should return HAPPY to pass test

--- Mood_Analyser/src/test/java/com/bridgelabz/moodAnalyser/MoodAnalyserTest.java
+++ Mood_Analyser/src/test/java/com/bridgelabz/moodAnalyser/MoodAnalyserTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
  */
 public class MoodAnalyserTest {
     MoodAnalyser moodAnalyzer = new MoodAnalyser();     // created moodAnalyzer object of class MoodAnalyser
-    @Test
     /**
      *  created analyseMoodTest method to test analyseMood method for SAD mood
      */
+    @Test
     public void analyseMoodTest() {
         /*
-         *  passing String "I am in Sad mood" as parameter in analyseMood method
-         *  checking expected (SAD) and actual output are equal or not
+         *  passing String "I am in Any mood" as parameter in analyseMood method
+         *  checking expected (HAPPY) and actual output are equal or not
          *  using assertEquals method of class Assertions
          *  if equal then Test passed otherwise failed
          */
-        String mood = moodAnalyzer.analyseMood("I am in Sad mood");
-        Assertions.assertEquals("SAD", mood);
+        String mood = moodAnalyzer.analyseMood("I am in Any mood");
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
