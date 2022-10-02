0215b146b7e3fde1787f12922cea525f3796aaa3
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-02 23:35:30 +0530

    [Shreyash]: Added code to add enam to differentiate error type null or empty mood

---
+++ Mood_Analyser/src/main/java/com/bridgelabz/moodAnalyser/MoodAnalysisErrors.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.moodAnalyser;
+
+/**
+ * @author - Shreyash Jadhav
+ */
+public enum MoodAnalysisErrors {
+    /*
+     *  enum is special class that represents group of constants
+     */
+    NULL, EMPTY;
+}
